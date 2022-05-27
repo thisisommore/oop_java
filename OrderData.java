198f63cce0ae09974049b18d5f65e9c1e0b4b01d
thisisommore <ommore501@gmail.com>
2022-05-27 02:51:52 +0000

    Implement OrderData according to new requirements

--- OrderData.java
+++ OrderData.java
@@ -1,69 +1,42 @@
 import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.List;
 
 class OrderData {
+
+    public static List<String> getNames(List<Order> orders) {
+        List<String> itemNames = new ArrayList<String>();
+        for (Order order : orders) {
+            itemNames.addAll(order.itemNames);
+        }
+        return itemNames;
+    }
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter number of orders - ");
-        int noOfOrders = in.nextInt();
-        ArrayList<Order> orders = new ArrayList<Order>(noOfOrders);
-        for (int index = 0; index < noOfOrders; index++) {
-            System.out.print("Enter number of items - ");
-            int noOfItems = in.nextInt();
-            // Consume new line
-            in.nextLine();
+        List<Order> orders = new ArrayList<Order>();
+        List<String> items1 = new ArrayList<String>();
+        items1.add("TShirt");
+        items1.add("Jeans");
+        items1.add("Shirt");
+        List<String> items2 = new ArrayList<String>();
+        items2.add("Pant");
+        items2.add("Socks");
+        items2.add("Tie");
+        orders.add(new Order(101, items1, true));
+        orders.add(new Order(102, items2, true));
 
-            ArrayList<String> items = new ArrayList<String>(noOfItems);
-            for (int j = 0; j < noOfItems; j++) {
-                System.out.printf("%d : Enter item name ", j + 1);
-                items.add(in.nextLine());
-            }
-            System.out.print("Do you want COD [Y/N] - ");
-            String wantCod = in.nextLine();
-            Boolean boolWantCod = false;
-            if (wantCod.equals("Y")) {
-                boolWantCod = true;
-            } else if (wantCod.equals("N")) {
-                boolWantCod = false;
-            } else {
-                throw new InputMismatchException("Only Y and N are accepted");
-            }
-            orders.add(new Order(items, boolWantCod));
-        }
-        for (Order _order : orders) {
-            System.out.println(_order);
-        }
-        in.close();
-
+        System.out.println(getNames(orders));
     }
 }
 
 class Order {
-    static int counter = 0;
     public int orderId;
-    public ArrayList<String> items;
+    public List<String> itemNames;
     public Boolean COD;
 
-    Order(ArrayList<String> _items, Boolean _COD) {
-        orderId = counter + 1;
-        counter++;
-        items = _items;
-        COD = _COD;
-    }
-
-    public String toString() {
-        String isCod = COD ? "Yes" : "No";
-        String result = String.format(
-                "Order id: %d\n" +
-                        "Is COD: %s\n",
-                orderId, isCod);
-        StringBuilder builder = new StringBuilder(result);
-        for (String item : items) {
-            builder.append(item + " ");
-        }
-        builder.append("\n");
-        return builder.toString();
+    Order(int orderId, List<String> itemNames, Boolean COD) {
+        this.orderId = orderId;
+        this.itemNames = itemNames;
+        this.COD = COD;
     }
 
 }
